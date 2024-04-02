dade125846e2d833018ed841ff0c209e59bb64f5
AJAY KANDPAL <ajay.kandpal1998@gmail.com>
2024-04-02 12:29:45 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,20 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "children" }, [
+    React.createElement("h1", { id: "heading" }, "Hi Nested Element"),
+    React.createElement("h1", { id: "heading" }, "Hi Nested Element 2"),
+  ])
+);
+console.log(parent);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", abc: "xyz" },
+//   "Hello World from React"
+// );
+
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
