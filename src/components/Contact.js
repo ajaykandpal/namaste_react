10eb30d6146758eadc433d6b142337f446f3ca12
AJAY KANDPAL <ajay.kandpal1998@gmail.com>
2024-04-11 11:27:33 +0530

    adding test setup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,21 @@ const Contact = () => {
       <h1>Contact Us</h1>
       <h2>Phone No: 7467802509</h2>
       <h2>Email: ajay@gmail.com</h2>
+      <form>
+        <input
+          className='border border-black p-2 m-2'
+          type='text'
+          placeholder='name'
+        ></input>
+        <input
+          className='border border-black p-2 m-2'
+          type='text'
+          placeholder='message'
+        ></input>
+        <button className='border border-black bg-gray-100 rounded-xl p-2 m-2'>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
